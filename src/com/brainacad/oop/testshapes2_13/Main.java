a6f2a379b3af78180c8d65380f85e05978d6b8fe
Roman Mazurov <mazurov.rn@gmail.com>
2016-03-13 23:37:00 +0200

    comleated 2.13.4 and 2.13.5 labs

--- src/com/brainacad/oop/testshapes2_13/Main.java
+++ src/com/brainacad/oop/testshapes2_13/Main.java
@@ -1,11 +1,12 @@
 package com.brainacad.oop.testshapes2_13;
 
+import java.util.Scanner;
+
 /**
  * Created by R2-D2 on 28.02.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        //Lab 2.13.3
         try {
             Rectangle rectangle2_10_4 = (Rectangle) Shape.parseShape("Rectangle:RED:10,20");
             System.out.println(rectangle2_10_4);
@@ -23,6 +24,21 @@ public class Main {
             System.out.println(circle2_10_4);
         } catch (InvalidShapeStringException e) {
             e.printStackTrace();
+        }
+        // Lab2.13.5
+        Scanner scanner = new Scanner(System.in);
+        int quantityOfShapes = Integer.parseInt(scanner.nextLine());
+        Shape[] shapes = new Shape[quantityOfShapes];
+        for (int i = 0; i < quantityOfShapes; i++) {
+            String newShape = scanner.nextLine();
+            try {
+                shapes[i] = Shape.parseShape(newShape);
+            } catch (InvalidShapeStringException e) {
+                e.printStackTrace();
+            }
+        }
+        for (Shape shape : shapes) {
+            shape.draw();
         }
     }
 }
